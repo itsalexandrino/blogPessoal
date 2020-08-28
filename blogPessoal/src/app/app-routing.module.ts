8ce433c12ec5c73f4b8ba21478c21c633350ab21
Matheus Alexandrino <malexandrino14@gmail.com>
2020-08-28 09:21:29 -0300

    atividade de rotas

--- blogPessoal/src/app/app-routing.module.ts
+++ blogPessoal/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FeedComponent } from './feed/feed.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch:'full'},
+  {path: 'home', component:HomeComponent},
+  {path: 'feed', component:FeedComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
